0205f4816f57339184347a34845ef8a1ea914010
KienPhan <kien1998tqz2@gmail.com>
2025-03-05 16:31:48 -0500

    Add compare theory vs simulation ftau

--- chromosomes.py
+++ chromosomes.py
@@ -4,14 +4,16 @@ import math
 
 N = 100
 
+
 def function(n, t, k):
     return k * math.factorial(N) / (np.math.factorial(n) * np.math.factorial(N-n-1))\
-              * (np.exp(-(n+1)*k*t))*(1-np.exp(-k*t))**(N-n-1)
+        * (np.exp(-(n+1)*k*t))*(1-np.exp(-k*t))**(N-n-1)
+
 
 def plot_chromosomes():
     t = np.linspace(0, 60, 100)
     k_values = [0.1, 0.2, 0.3, 0.4]  # Choose 4 values of k
-    
+
     fig, axs = plt.subplots(2, 2, figsize=(12, 8))
     axs = axs.flatten()  # Flatten the 2D array of axes for easy iteration
     for i, k in enumerate(k_values):
@@ -19,8 +21,10 @@ def plot_chromosomes():
             axs[i].plot(t, function(n, t, k), label=f'n={n}')
         axs[i].legend()
         axs[i].set_title(f'Plot for k={k}')
-    
+
     plt.tight_layout()
     plt.show()
 
-plot_chromosomes()
+
+if __name__ == "__main__":
+    plot_chromosomes()
