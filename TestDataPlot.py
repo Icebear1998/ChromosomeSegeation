1f20f1f74cd3d53e099482bf3dccdc94f0925801
KienPhan <kien1998tqz2@gmail.com>
2025-03-19 16:31:05 -0400

    fix merge error

--- TestDataPlot.py
+++ TestDataPlot.py
@@ -63,36 +63,32 @@ def run_stochastic_simulation_and_plot(k_opt, r21_opt, R21_opt, r23_opt, R23_opt
 
 if __name__ == "__main__":
 
-df = pd.read_excel("Data/Chromosome_diff.xlsx")
-data12 = df['Wildtype12'].dropna().values
-data32 = df['Wildtype32'].dropna().values
+    df = pd.read_excel("Data/Chromosome_diff.xlsx")
+    data12 = df['Wildtype12'].dropna().values
+    data32 = df['Wildtype32'].dropna().values
 
-x_grid = np.linspace(-80, 80, 301)
+    x_grid = np.linspace(-80, 80, 301)
 
-n2_opt = 10.00
-N2_opt = 100
-k_opt = 0.0283
-r21_opt = 0.5
-R21_opt = 0.50
-r23_opt = 0.9
-R23_opt = 1.25
-n1_opt = r21_opt * n2_opt
-N1_opt = R21_opt * N2_opt
-n3_opt = r23_opt * n1_opt
-N3_opt = R23_opt * N1_opt
+    n2_opt = 10.00
+    N2_opt = 100
+    k_opt = 0.0283
+    r21_opt = 0.5
+    R21_opt = 0.50
+    r23_opt = 0.9
+    R23_opt = 1.25
+    n1_opt = r21_opt * n2_opt
+    N1_opt = R21_opt * N2_opt
+    n3_opt = r23_opt * n1_opt
+    N3_opt = R23_opt * N1_opt
 
- pdf12 = np.array([
-      f_diff(x, k_opt, n1_opt, N1_opt, n2_opt, N2_opt)
-      for x in x_grid
-      ])
-  area12 = np.trapz(pdf12, x_grid)
-   if area12 > 1e-15:
+    pdf12 = np.array(
+        [f_diff(x, k_opt, n1_opt, N1_opt, n2_opt, N2_opt) for x in x_grid])
+    area12 = np.trapz(pdf12, x_grid)
+    if area12 > 1e-15:
         pdf12 /= area12
 
-    pdf32 = np.array([
-        f_diff(x, k_opt, n3_opt, N3_opt, n2_opt, N2_opt)
-        for x in x_grid
-    ])
+    pdf32 = np.array(
+        [f_diff(x, k_opt, n3_opt, N3_opt, n2_opt, N2_opt)for x in x_grid])
     area32 = np.trapz(pdf32, x_grid)
     if area32 > 1e-15:
         pdf32 /= area32
